862c7cbdd99377cdbfbf2eed5065e24ade7b52d7
Lawalhamzat11 <lawalanuoluwapo11@gmail.com>
2022-09-08 10:22:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: using putchar function
+ * this program print and that piece ofart is useful" - Dora Korpar, 2015-10-19
+ * Return: 1 on Success
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
