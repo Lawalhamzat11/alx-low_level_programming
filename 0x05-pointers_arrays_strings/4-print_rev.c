79e694c58148cc4f8aaf87cd521898100aebc977
Lawalhamzat11 <lawalanuoluwapo11@gmail.com>
2022-09-20 14:05:44 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int index;
 
 	while (s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index = index - 1; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
