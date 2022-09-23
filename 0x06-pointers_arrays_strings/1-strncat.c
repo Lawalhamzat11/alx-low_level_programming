5a341947a0fd47b26b0fbb0d27a5ed062c35d5d2
Lawalhamzat11 <lawalanuoluwapo11@gmail.com>
2022-09-22 18:54:17 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+Include "main.h"
+
+/**
+ * _strncat - function to append some charx
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: third parameter
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	_strncat(dest, src, n);
+	return (dest);
+}
