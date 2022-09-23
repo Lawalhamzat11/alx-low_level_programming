5d9405c6ff2f4a5f7af2bfb5c22b7d482c40cb55
Lawalhamzat11 <lawalanuoluwapo11@gmail.com>
2022-09-22 18:34:57 -0700

    The best

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat -  this is the function strcat
+ * @dest: first parameter
+ * @src: second parameter
+ * Return: a string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	int j;
+
+	while (dest[len])
+		len++;
+
+	for (j =0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	reutrn (dest);
+}
