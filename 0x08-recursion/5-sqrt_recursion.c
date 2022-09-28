564f400c2360e98e9824b7d6c6dfaac51dc8ca46
Lawalhamzat11 <lawalanuoluwapo11@gmail.com>
2022-09-28 09:22:02 -0700

    Task 5 Updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,47 @@
-#include "main"
-
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
+#include "main.h"
+int _root_sqrt(int raiz, int cont);
 
 /**
- * find_sqrt - Finds the natural square root of an inputted number.
- * @num: The number to find the square root of.
- * @root: The root to be tested.
+ * _sqrt_recursion - function that returns the natural square root of a number
+ *@n: first value
  *
- * Return: If the number has a natural square root - the square root.
- *         If the number does not have a natural square root - -1.
- */
-int find_sqrt(int num, int root)
-{
-	if ((root * root) == num)
-		return (root);
-
-	if (root == num / 2)
-		return (-1);
-
-	return (find_sqrt(num, root + 1));
-}
-
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to return the square root of.
- *
- * Return: If n has a natural square root - the natural square root of n.
- *         If n does not have a natural square root - -1.
+ * Return: Return solution
  */
 int _sqrt_recursion(int n)
 {
-	int root = 0;
-
+	if (n == 0)
+	{
+		return (0);
+	}
 	if (n < 0)
+	{
 		return (-1);
+	}
+	else
+	{
+		return (_root_sqrt(n, 1));
+	}
+}
 
-	if (n == 1)
-		return (1);
-
-	return (find_sqrt(n, root));
+/**
+ * _root_sqrt - calculate the square root
+ *@raiz: value to calc the square root
+ *@cont: variable to cont
+ *
+ * Return: the number with the solution
+ */
+int _root_sqrt(int raiz, int cont)
+{
+	if (cont * cont == raiz)
+	{
+		return (cont);
+	}
+	else if (cont * cont > raiz)
+	{
+	return (-1);
+	}
+	else
+	{
+		return (_root_sqrt(raiz, cont + 1));
+	}
 }
