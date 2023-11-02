e13418564bb088a1db923936866e0d348aeed22f
Lawalhamzat <lawalanuoluwapo11@gmail.com>
2023-11-02 23:29:28 +0000

    fixed bug

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	new = malloc(nmemb * size);
 	if (new == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*((char *)(new) + i) = 0;
 	return (new);
 }
