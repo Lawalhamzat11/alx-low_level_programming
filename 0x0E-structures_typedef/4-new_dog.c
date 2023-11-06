029b545b8c18b0eab2be2971693c9387b4fe6f62
Lawalhamzat <lawalanuoluwapo11@gmail.com>
2023-11-06 23:25:53 +0000

    betty

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,10 @@
 #include "dog.h"
-
 /**
  * new_dog - create a new struct
  * @name: namento be initialized
  * @age: age.to be initialized
  * @owner: name of owner to.be initialized
- * Return; the address of the new struct created
+ * Return: the address of the new struct created
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -18,11 +17,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (!new->name)
 		return (NULL);
 	new->name = strdup(name);
-	new->age = age;	
-	new->owner= malloc((strlen(owner) + 1) * sizeof(char *));
+	new->age = age;
+	new->owner = malloc((strlen(owner) + 1) * sizeof(char *));
 	if (new->owner == NULL)
 		return (NULL);
 	new->owner = strdup(owner);
-
 	return (new);
 }
