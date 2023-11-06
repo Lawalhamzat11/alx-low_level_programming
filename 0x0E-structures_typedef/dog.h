fd366e47d3285d9abef88bd1f3fc930db80dad59
Lawalhamzat <lawalanuoluwapo11@gmail.com>
2023-11-06 21:11:49 +0000

    betty errors

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog - a struct for information about a dog
+ * struct dog - a struct for information about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog's name
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} ;
+};
 #endif
