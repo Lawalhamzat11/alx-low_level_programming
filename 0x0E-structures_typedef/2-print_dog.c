30d05d1e75d62e7ed3a6917bc3be5bb73ac5827d
Lawalhamzat <lawalanuoluwapo11@gmail.com>
2023-11-06 23:26:53 +0100

    print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * pront_dog - prints a struct dog
+ * @d: struct to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+		return;
+	if (!d->name)
+		d->name = "(nil)";
+	if(!d->owner)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - a struct for information about a dog
  * @name: dog's name
@@ -14,4 +15,5 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
