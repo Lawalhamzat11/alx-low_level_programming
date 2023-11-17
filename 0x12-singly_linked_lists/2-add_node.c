7978d753b7730e6ce006304716d59259cd5114ac
nunu <lawalanuoluwapo11@gmail.com>
2023-11-17 01:44:09 +0000

    hello

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+  * add_node - adds a new node at the beginning of a list_t list.
+  * @head: address that points to a linked list
+  * @str: string to be duplicated
+  * Return: the address of the new element
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	tmp = malloc(sizeof(list_t));
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	tmp->str = strdup(str);
+	if (tmp->str == NULL)
+	{
+		return (NULL);
+	}
+	tmp->len = strlen(str);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+
+}
