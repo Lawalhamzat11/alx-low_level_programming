bc554f95302692338aa155746f5026352192716f
nunu <lawalanuoluwapo11@gmail.com>
2023-11-28 03:59:25 +0000

    updated' '

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,11 +12,12 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_WRONLY | O_APPEND);
-	if (file == -1)
-		return (-1);
+
 	if (text_content != NULL)
 	{
+		file = open(filename, O_WRONLY | O_APPEND);
+		if (file == -1)
+			return (-1);
 		check_write = write(file, text_content, strlen(text_content));
 		if (check_write == -1)
 		{
@@ -24,9 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			return (-1);
 		}
 		close(file);
-		return (1);
 	}
-	close(file);
 	return (1);
 
 }
