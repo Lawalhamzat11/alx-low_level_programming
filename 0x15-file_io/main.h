4749d4154cd3ca28f9665b4c4add170ea4f33953
nunu <lawalanuoluwapo11@gmail.com>
2023-11-28 04:30:02 +0000

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,19 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <elf.h>
+
+void check_elf(unsigned char *_e_ident);
+void print_magic(unsigned char *_e_ident);
+void print_class(unsigned char *_e_ident);
+void print_data(unsigned char *_e_ident);
+void print_version(unsigned char *_e_ident);
+void print_os(unsigned char *_e_ident);
+void print_osabi(unsigned char *_e_ident);
+void print_type(unsigned int _e_type, unsigned char *_e_ident);
+void print_entry(unsigned long int _e_entry, unsigned char *_e_ident);
+void close_elf(int _elf);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
