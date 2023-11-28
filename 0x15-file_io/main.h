46ca826aa3e7b56209fe7e6cc15580dec9fecb51
nunu <lawalanuoluwapo11@gmail.com>
2023-11-28 03:05:57 +0000

    Forgot my header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
